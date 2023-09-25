98a9cc8b8e17070c56547a8c9a5430d5fd36bbd9
Albert Albala <albert@mediatribe.net>
2023-09-25 19:17:14 -0400

    Use alpine for smaller image size (#4)

--- docker-resources/build.sh
+++ docker-resources/build.sh
@@ -1,8 +1,12 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Build the Docker image.
 #
 set -e
+
+# https://github.com/facebook/flow/issues/8846#issuecomment-1046616800
+apk add --no-cache gcompat
+apk add --no-cache libatomic
 
 mkdir -p /app/code
 cp /docker-resources/package.json /app/package.json
--- test.sh
+++ test.sh
@@ -1,11 +1,12 @@
+#!/bin/bash
 set -e
-docker pull node
+docker pull node:alpine
 docker build -t local-dcycle-flow-image .
 
 echo "Make sure code passes"
 docker run --rm -v $(pwd)/example01:/app/code local-dcycle-flow-image
 echo "Make sure code fails"
 ! docker run --rm -v $(pwd)/example02:/app/code local-dcycle-flow-image
-docker run --rm -v $(pwd)/example03:/app/code local-dcycle-flow-image
-docker run --rm -v $(pwd)/example04:/app/code local-dcycle-flow-image
+! docker run --rm -v $(pwd)/example03:/app/code local-dcycle-flow-image
+! docker run --rm -v $(pwd)/example04:/app/code local-dcycle-flow-image
 echo "All done with tests!"
