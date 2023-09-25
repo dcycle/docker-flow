123a5f88713ced64bd17a4553448c018ff98990c
Albert Albala <albert@mediatribe.net>
2023-09-25 15:02:46 -0400

    Please squash this commit in the pull request

--- docker-resources/build.sh
+++ docker-resources/build.sh
@@ -4,6 +4,10 @@
 #
 set -e
 
+# https://github.com/facebook/flow/issues/8846#issuecomment-1046616800
+apk add --no-cache gcompat
+apk add --no-cache libatomic
+
 mkdir -p /app/code
 cp /docker-resources/package.json /app/package.json
 cp /docker-resources/.flowconfig /app/.flowconfig
